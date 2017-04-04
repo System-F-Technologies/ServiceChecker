053298b2de21449904d1e3fbb6e3c6f8d91073fa
Drew Hoo <drewhoo@drewhoos-mbp.bc.edu>
2017-04-04 17:27:55 -0400

    Initial Commit

---
+++ SystemChecker2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SystemChecker2
+//
+//  Created by Drew Hoo on 4/4/17.
+//  Copyright © 2017 System F Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
